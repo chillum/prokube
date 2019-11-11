8080beb8ae38d87bfaf8176e98088c39c68ad88b
Vasily Korytov <v.korytov@outlook.com>
2019-11-11 22:33:46 +0200

    obvious control flow, I just can't stand it

--- controller-down.sh
+++ controller-down.sh
@@ -2,7 +2,7 @@
 echo "Bringing down controller manager. Press Ctrl-C to bring it back."
 trap "sudo mv /tmp/kube-controller-manager.yaml /etc/kubernetes/manifests/; exit 0" 2
 
-sudo mv /etc/kubernetes/manifests/kube-controller-manager.yaml /tmp/
+sudo mv /etc/kubernetes/manifests/kube-controller-manager.yaml /tmp/ || exit 1
 sleep 1m
 
 echo -n "\a" # beep
