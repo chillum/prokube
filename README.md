7597f77bfcce16902bc496eb25276e3dde64b5d5
Vasily Korytov <v.korytov@outlook.com>
2019-11-11 20:27:13 +0200

    third alert

--- README.md
+++ README.md
@@ -12,4 +12,5 @@ Get Vagrant and VirtualBox, fire `vagrant up`. It's fully automated.
 ## Test cases for alerts
 
 * Kube controller down: enter master and launch `/vagrant/controller-down.sh`
-* CPU usage: enter any node and launch `/vagrant/load-cpu.sh`
+* CPU usage: enter any node and launch `/vagrant/load-cpus.sh`
+* non-Pod CPU usage: enter any node and launch `/vagrant/load-cpu.sh`
--- load-cpus.sh
+++ load-cpus.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
-echo "Starting CPU stress test, will generate 100% load. Press Ctrl-C to abort."
+echo "Starting CPU stress test, will generate 200% load. Press Ctrl-C to abort."
 trap "killall yes; exit 0" 2
 
+yes >/dev/null &
 yes >/dev/null &
 
 sleep 15m
 sleep 10 # ten more seconds for alert to fire for sure
 echo -n "\a" # beep
 echo "Check Prometheus for alerts."
-wait %1
+wait %1 %2
