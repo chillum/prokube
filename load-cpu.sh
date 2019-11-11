bec3980cba63df5b8f8cd1273c1c2915e3fc4215
Vasily Korytov <v.korytov@outlook.com>
2019-11-11 22:27:41 +0200

    formatting to improve easier reading

--- load-cpu.sh
+++ load-cpu.sh
@@ -3,8 +3,8 @@ echo "Starting CPU stress test, will generate 100% load. Press Ctrl-C to abort."
 trap "killall yes; exit 0" 2
 
 yes >/dev/null &
-
 sleep 15m
+
 sleep 10 # ten more seconds for alert to fire for sure
 echo -n "\a" # beep
 echo "Check Prometheus for alerts."
--- load-cpus.sh
+++ load-cpus.sh
@@ -4,8 +4,8 @@ trap "killall yes; exit 0" 2
 
 yes >/dev/null &
 yes >/dev/null &
-
 sleep 15m
+
 sleep 10 # ten more seconds for alert to fire for sure
 echo -n "\a" # beep
 echo "Check Prometheus for alerts."
